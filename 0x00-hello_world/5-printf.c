2c284ef4b404021d8288000baa59e1e0df9dd095
robelyes <robelyeshiwas93@gmail.com>
2022-09-08 17:35:29 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*my hello print task
+*Return: Always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
