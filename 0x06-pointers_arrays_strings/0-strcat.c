b422789087513a2942114671d0882be1e6187cd9
robelyes <robelyeshiwas93@gmail.com>
2022-09-22 03:37:54 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,6 @@
-*_strcat - concatenates two strings
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
  *@dest: A pointer to a character that will be changed
  *@src: A pointer to a character that will also be changed
  *Return: dest
